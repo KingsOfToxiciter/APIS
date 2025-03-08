3f7337d18abb56df158264453f3199a1d538985a
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-09 05:59:17 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get update && apt-get install -y wget
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
