d8cf7342b0058a771369834496d189abd77decad
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-09 06:28:05 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p /tmp/yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /tmp/yt-dlp/yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /tmp/yt-dlp/yt-dlp
 chmod +x /tmp/yt-dlp/yt-dlp
-ls -lah /tmp/yt-dlp/
